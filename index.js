c97ed22e8293eff877eee64ec1bbb8e3e814c17a
unverzed <vitoriarortega@gmail.com>
2022-05-03 05:44:03 -0300

    feat: connecting with database

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ import dotenv from "dotenv";
 import joi from "joi";
 import { MongoClient } from "mongodb";
 
-
 const app = express();
 app.use(cors());
 dotenv.config();
 app.use(express.json());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect().then(() => {
+  db = mongoClient.db("my_wallet");
+  console.log("Conectado ao banco de dados com sucesso");
+});
+
+app.listen(5000, () => console.log("Server working"));
