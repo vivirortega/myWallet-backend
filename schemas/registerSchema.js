1fd715c6b59400fb90026df40f7ba3aac5a4716f
Guga <contato.gustavobarbosa@outlook.com>
2022-05-17 15:15:02 -0300

    Preparando revisão de código

--- schemas/registerSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-
-const registerSchema = joi.object({
-  date: joi.number().integer().required(),
-  value: joi.number().required(),
-  description: joi.string().required()
-});
-
-export default registerSchema;
